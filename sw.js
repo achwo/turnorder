42d1b3c932de3c06856eacb76c963184cb1a904f
Felix Jensen <felix.jensen@mindmatters.de>
2018-04-30 18:08:50 +0200

    wrap if screen is too small

--- sw.js
+++ sw.js
@@ -18,11 +18,8 @@ var contentToCache = appShellFiles.concat(cardsImages);
 
 // Install serviceWorker
 self.addEventListener('install', function(e) {
-  console.log('[Service Worker] Install');
-
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      console.log('[Service Worker] Caching all: app shell and content');
       return cache.addAll(contentToCache);
     })
   );
@@ -33,10 +30,8 @@ self.addEventListener('install', function(e) {
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(r) {
-      console.log('[Service Worker] Fetching resource: ' + e.request.url);
       return r || fetch(e.request).then(function(response) {
         return caches.open(cacheName).then(function(cache) {
-          console.log('[Service Worker] Caching new resource: ' + e.request.url);
           cache.put(e.request, response.clone());
 
           return response;
